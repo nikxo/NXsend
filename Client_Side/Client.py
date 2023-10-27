833257c37ee1b501d0432edfc069c7ea0a666cc1
Nicolas Beaudet <razermenyt@gmail.com>
2023-10-27 18:46:26 +0200

    ASCII for menu, help ,settings

--- Client_Side/Client.py
+++ Client_Side/Client.py
@@ -3,7 +3,7 @@ import fonction
 # Adresse serveur
 IP_Server = '172.31.190.240'
 Port_Server = 12345
-Menu = ["Help", "Settings",  "Exit"]
+Menu = ["Help", "Settings", "Exit"]
 
 
 # Creation Socket de connection
@@ -18,6 +18,13 @@ while True:
     try:
         if (input_.lower() == 'menu'):
             fonction.menu(Menu)
+
+        if (input_.lower() == 'settings'):
+            fonction.settings()
+
+        if (input_.lower() == 'help'):
+            fonction.help()
+
         if (input_.lower() == 'exit'):
             socket_.close()
             break
